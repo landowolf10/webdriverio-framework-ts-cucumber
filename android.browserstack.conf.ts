fd05657da998f3c859dddda0a8e383feecc711ab
Orlando Avila <orlando.avilag@hotmail.com>
2024-04-18 13:28:07 -0600

    Adding CircleCI environment variables.

--- android.browserstack.conf.ts
+++ android.browserstack.conf.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import * as YAML from 'yaml';
 
 //let doc = YAML.readFileSync(fs.readFileSync('./github/workflows/ci.yml', 'utf8'));
-const configFile = fs.readFileSync('./github/workflows/ci.yml', 'utf-8');
+const configFile = fs.readFileSync('./.github/workflows/ci.yml', 'utf-8');
 YAML.parse(configFile);
 //console.log(doc.admin.permissions);
 
